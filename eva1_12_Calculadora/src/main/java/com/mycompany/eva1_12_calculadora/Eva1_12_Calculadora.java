d491b0425da9e84880f8ccd00b9790e70960cbdc
Jesus Alexis Jimenez Aldaba <aldabaalexis85@gmail.com>
2025-02-18 20:29:19 -0600

    Add files via upload

---
+++ eva1_12_Calculadora/src/main/java/com/mycompany/eva1_12_calculadora/Eva1_12_Calculadora.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_12_calculadora;
+
+import static com.mycompany.eva1_12_calculadora.CALCULADORA.Sumar;
+
+/**
+ *
+ * @author Alexis
+ */
+public class Eva1_12_Calculadora {
+
+    public static void main(String[] args) {
+        System.out.println(CALCULADORA.Sumar(5, 3));
+          System.out.println(CALCULADORA.Restar(5, 3));
+           System.out.println(CALCULADORA.Multiplicar(5, 3));
+            System.out.println(CALCULADORA.Dividir(5, 3));
+             System.out.println(CALCULADORA.Potencia(5, 3));
+    }
+}
